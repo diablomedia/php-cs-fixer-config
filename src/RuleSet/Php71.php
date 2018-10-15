9b5610865557681aa07bf46227907614f975c97f
Jay Klehr <jay@diablomedia.com>
2018-10-15 15:53:10 -0600

    adding psr0 fixer, alphabetizing

--- src/RuleSet/Php71.php
+++ src/RuleSet/Php71.php
@@ -9,26 +9,29 @@ final class Php71 extends AbstractRuleSet
     protected $name = 'diablomedia (PHP 7.1)';
 
     protected $rules = [
+        // Pre-defined rule sets
         '@PSR2' => true,
         '@PHP70Migration' => true,
         '@PHP71Migration' => true,
         '@PHP71Migration:risky' => true,
         '@PHPUnit60Migration:risky' => true,
+        // Individual rules
+        'array_syntax' => ['syntax' => 'short'],
         'binary_operator_spaces' => [
             'default' => 'align_single_space_minimal',
             'operators' => ['||' => null, '&&' => null, '==' => null, '===' => null, '+' => null]
         ],
-        'single_quote' => false,
-        'array_syntax' => ['syntax' => 'short'],
         'concat_space' => ['spacing' => 'one'],
-        'dir_constant' => true,
-        'psr0' => true,
-        'no_unused_imports' => true,
         'declare_strict_types' => false,
-        'return_type_declaration' => ['space_before' => 'none'],
-        'no_superfluous_phpdoc_tags' => true,
+        'dir_constant' => true,
         'no_empty_comment' => true,
         'no_empty_phpdoc' => true,
+        'no_mixed_echo_print' => ['use' => 'echo'],
+        'no_superfluous_phpdoc_tags' => true,
+        'no_unused_imports' => true,
+        'psr0' => true,
+        'return_type_declaration' => ['space_before' => 'none'],
+        'single_quote' => false,
     ];
 
     protected $targetPhpVersion = 70100;
