8f6726f9921e026b660aad5185bfdcc47e2116bc
Jay Klehr <jay@diablomedia.com>
2021-03-11 13:30:55 -0700

    updating base lib, removing vendor dir

--- src/RuleSet/Php71.php
+++ src/RuleSet/Php71.php
@@ -2,7 +2,7 @@
 
 namespace DiabloMedia\PhpCsFixer\Config\RuleSet;
 
-use Localheinz\PhpCsFixer\Config\RuleSet\AbstractRuleSet;
+use Ergebnis\PhpCsFixer\Config\RuleSet\AbstractRuleSet;
 
 final class Php71 extends AbstractRuleSet
 {
@@ -21,48 +21,14 @@ final class Php71 extends AbstractRuleSet
             'default' => 'align_single_space_minimal',
             'operators' => ['||' => null, '&&' => null, '==' => null, '===' => null, '+' => null]
         ],
-        'class_attributes_separation' => true,
         'concat_space' => ['spacing' => 'one'],
         'declare_strict_types' => false,
         'dir_constant' => true,
-        'lowercase_static_reference' => true,
         'no_empty_comment' => true,
         'no_empty_phpdoc' => true,
-        'no_leading_import_slash' => true,
         'no_mixed_echo_print' => ['use' => 'echo'],
-        'no_superfluous_phpdoc_tags' => ['allow_mixed' => true],
-        'no_unneeded_control_parentheses' => true,
-        'no_unneeded_curly_braces' => true,
+        'no_superfluous_phpdoc_tags' => true,
         'no_unused_imports' => true,
-        'no_useless_return' => true,
-        'ordered_class_elements' => [
-            'order' => [
-                'use_trait',
-                'constant_public',
-                'constant_protected',
-                'constant_private',
-                'property_public',
-                'property_protected',
-                'property_private',
-                'construct',
-                'destruct',
-                'magic',
-                'phpunit',
-                'method_public',
-                'method_protected',
-                'method_private'
-            ],
-            'sortAlgorithm' => 'alpha'
-        ],
-        'ordered_imports' => [
-            'imports_order' => [
-                'const',
-                'class',
-                'function',
-            ],
-            'sort_algorithm' => 'alpha'
-        ],
-        'php_unit_construct' => true,
         'psr0' => true,
         'return_type_declaration' => ['space_before' => 'none'],
         'single_quote' => false,
